294c85757fff5509b9ea3d3718b76617d7614f88
Philip Stachowski <shiznit0587@me.com>
2017-12-07 23:16:52 -0500

    What I was able to figure out, woefully incomplete

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,19 @@
-let () = print_endline "Hello, World!"
+let input = "1122";;
+
+let firstchar = 1122;;
+
+(* how do I parse characters out of a string? *)
+
+let int = int_of_string input;;
+
+(* Okay, I have an int representing the first char. how do I loop? *)
+
+let day1 = (
+    Printf.printf "%d\n" int;
+);;
+
+let () = (
+    (* Example Comment *)
+    Printf.printf "%s\n" "Hello, World!";
+    day1
+);;

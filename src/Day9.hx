a37ec1b36fd54f7eb0b46aafca4725ae8e42c2a6
Philip Stachowski <shiznit0587@me.com>
2017-12-24 16:12:49 -0500

    Finished Day 9-b

--- src/Day9.hx
+++ src/Day9.hx
@@ -3,62 +3,50 @@ class Day9 {
     public function new() {
         neko.Lib.println("Running Day 9 - a");
 
+        var input:String = null;
         FileUtil.readInput("9", function (line) {
+            input = line;
+        });
 
-            //neko.Lib.println('input = $line');
+        var iter:CharIterator = new CharIterator(input);
+        var skipGarbage:Bool = false;
+        var totalScore:Int = 0;
+        var groupCount:Int = 0;
+        var garbageCount:Int = 0;
 
-            var iter:CharIterator = new CharIterator(line);
-            var skipGarbage:Bool = false;
-            var totalScore:Int = 0;
-            var groupCount:Int = 0;
+        while (iter.hasNext()) {
 
-            while (iter.hasNext()) {
-
-                var char:String = iter.next();
-
-                if (skipGarbage) {
-                    switch (char) {
-                        case "!":
-                            if (iter.hasNext()) {
-                                //neko.Lib.println('$char : Skipping next character of ${iter.next()}');
-                                iter.next();
-                            }
-                        case ">":
-                            //neko.Lib.println('$char : Ending garbage');
-                            skipGarbage = false;
-                        default:
-                            //neko.Lib.println('$char : Skipping garbage');
-                    }
-                    continue;
-                }
-
-                switch (char) {
-                    case "{":
-                        ++groupCount;
-                        //neko.Lib.println('$char : Starting group - groupCount = $groupCount');
-                    case "}":
-                        totalScore += groupCount;
-                        --groupCount;
-                        //neko.Lib.println('$char : Ending group - score = ${groupCount+1}, totalScore = $totalScore, groupCount = $groupCount');
+            if (skipGarbage) {
+                switch (iter.next()) {
                     case "!":
                         if (iter.hasNext()) {
-                            //neko.Lib.println('$char : Skipping next character of ${iter.next()}');
                             iter.next();
                         }
-                    case "<":
-                        skipGarbage = true;
-                        //neko.Lib.println('$char : Starting garbage');
+                    case ">":
+                        skipGarbage = false;
                     default:
-                        //neko.Lib.println('$char : Unhandled character');
+                        ++garbageCount;
                 }
+                continue;
             }
 
-            neko.Lib.println('Total Score = $totalScore');
-        });
+            switch (iter.next()) {
+                case "{":
+                    ++groupCount;
+                case "}":
+                    totalScore += groupCount;
+                    --groupCount;
+                case "!":
+                    if (iter.hasNext()) {
+                        iter.next();
+                    }
+                case "<":
+                    skipGarbage = true;
+            }
+        }
 
+        neko.Lib.println('Total Score = $totalScore');
         neko.Lib.println("Running Day 9 - b");
-
-        FileUtil.readInput("9", function (line) {
-        });
+        neko.Lib.println('Garbage Count = $garbageCount');
     }
 }

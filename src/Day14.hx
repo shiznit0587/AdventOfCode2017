0dcd701c7caa9a2a3098632143c1c6d5765c8341
Philip Stachowski <shiznit0587@me.com>
2018-01-05 13:28:32 -0500

    Stubbing Day 14

---
+++ src/Day14.hx
@@ -0,0 +1,14 @@
+class Day14 {
+
+    public function new() {
+        neko.Lib.println("Running Day 14 - a");
+
+        FileUtil.readInput("14", function (line) {
+        });
+
+        neko.Lib.println("Running Day 14 - b");
+
+        FileUtil.readInput("14", function (line) {
+        });
+    }
+}

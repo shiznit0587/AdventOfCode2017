3ccea16191bd60130d8b0c4274b058f8fe27b396
Philip Stachowski <17034738+shiznit0587@users.noreply.github.com>
2024-12-02 14:36:05 -0500

    Updated Main

--- src/Main.hx
+++ src/Main.hx
@@ -2,21 +2,21 @@ class Main {
 
     public static function main() {
         neko.Lib.println(":: Advent of Code ::");
-        // new Day1();
-        // new Day2();
-        // new Day3();
-        // new Day4();
-        // new Day5();
-        // new Day6();
-        // new Day7();
-        // new Day8();
-        // new Day9();
-        // new Day10();
-        // new Day11();
-        // new Day12();
-        // new Day13();
-        // new Day14();
-        // new Day15();
+        new Day1();
+        new Day2();
+        new Day3();
+        new Day4();
+        new Day5();
+        new Day6();
+        new Day7();
+        new Day8();
+        new Day9();
+        new Day10();
+        new Day11();
+        new Day12();
+        new Day13();
+        new Day14();
+        new Day15();
         new Day16();
         new Day16_2();
         new Day16_3();
